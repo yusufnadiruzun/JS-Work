521fe610ca64f4862a9c3a8122951ce19e0bf441
Yusuf Nadir <69757325+yusufnadiruzun@users.noreply.github.com>
2021-12-27 22:05:08 +0300

    Update app.js

--- termProject/app.js
+++ termProject/app.js
@@ -16,7 +16,7 @@ currency.onchange = function writeCurrencyName(){
 
 async function returnValue(){
     var value = currency.options[currency.selectedIndex].value;
-    const url = "https://api.exchangeratesapi.io/v1/latest?access_key=d65d8547cccd0b3d45c7b59c070e58a6&format=1&symbols=" + value ;
+    const url = "http://api.exchangeratesapi.io/v1/latest?access_key=d65d8547cccd0b3d45c7b59c070e58a6&format=1&symbols=" + value ;
     const response = await fetch(url);
     const responsedata = await response.json();
     resultValue.innerHTML = Object.values(responsedata.rates)[0] * amount.value;
