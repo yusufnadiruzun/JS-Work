a69e7c11e1b0c5e0a592dfc0f2b9ae6c4d5b8c76
Yusuf Nadir <69757325+yusufnadiruzun@users.noreply.github.com>
2021-12-31 00:16:11 +0300

    Update app.js

--- termProject/app.js
+++ termProject/app.js
@@ -4,22 +4,35 @@
 const amount = document.querySelector("#money");
 const resutText = document.querySelector(".resultText");
 const resultValue = document.querySelector(".resultValue");
-const currency = document.getElementById("secondCurrency");
+const currency = document.getElementById("firstCurrency");
+const currency2 = document.getElementById("secondCurrency");
 
 
 currency.onchange = function writeCurrencyName(){
     var value = currency.options[currency.selectedIndex].value;
-   resutText.innerHTML = "EUR - " + value;
+    var secondValue = currency2.options[currency2.selectedIndex].value;
+   resutText.innerHTML =  value+" - " + secondValue;
+   returnValue();
+}
+
+currency2.onchange = function writeCurrency2Name(){
+    var value = currency.options[currency.selectedIndex].value;
+    var secondValue = currency2.options[currency2.selectedIndex].value;
+   resutText.innerHTML =  value+" - " + secondValue;
    returnValue();
 }
 
 
 async function returnValue(){
     var value = currency.options[currency.selectedIndex].value;
-    const url = "http://api.exchangeratesapi.io/v1/latest?access_key=d65d8547cccd0b3d45c7b59c070e58a6&format=1&symbols=" + value ;
+    var secondValue = currency2.options[currency2.selectedIndex].value;
+    const url = "https://free.currconv.com/api/v7/convert?q=" + value+"_" + secondValue+"&compact=ultra&apiKey=2306ece2d56b09cea72c";
+    console.log(url);
     const response = await fetch(url);
     const responsedata = await response.json();
-    resultValue.innerHTML = Object.values(responsedata.rates)[0] * amount.value;
+
+    console.log(Object.values(responsedata)[0]);
+    resultValue.innerHTML = Object.values(responsedata)[0] * amount.value;
     
 }
 
