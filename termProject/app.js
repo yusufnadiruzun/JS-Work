80b2f0eaa5b203778cc17ceda6c29fdfa457d677
Yusuf Nadir Uzun <69757325+yusufnadiruzun@users.noreply.github.com>
2022-01-16 20:04:47 +0300

    Update app.js

--- termProject/app.js
+++ termProject/app.js
@@ -6,20 +6,53 @@ const resutText = document.querySelector(".resultText");
 const resultValue = document.querySelector(".resultValue");
 const currency = document.getElementById("firstCurrency");
 const currency2 = document.getElementById("secondCurrency");
-
+const firstImage = document.getElementById("first");
+const secondImage = document.getElementById("second");
 
 currency.onchange = function writeCurrencyName(){
     var value = currency.options[currency.selectedIndex].value;
     var secondValue = currency2.options[currency2.selectedIndex].value;
    resutText.innerHTML =  value+" - " + secondValue;
+   if(value == "GBP"){
+    firstImage.src = "https://img2.pngindir.com/20180322/ide/kisspng-england-kingdom-of-great-britain-flag-of-the-unite-cyber-nations-wiki-5ab40918aab721.6894237315217482486993.jpg";
+   }
+   else if(value == "EUR"){
+    firstImage.src =   "https://ulkelerbayraklar.com/wp-content/uploads/2017/12/almanya_bayrag%CC%86%C4%B1-800x445.png";
+    }
+    else if(value == "USD"){
+        firstImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"
+    }
+    else if(value == "TRY"){
+        firstImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png"
+    }
+    else if(value == "JPY"){
+        firstImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/200px-Flag_of_Japan.svg.png"
+    }
    returnValue();
+   
 }
 
 currency2.onchange = function writeCurrency2Name(){
     var value = currency.options[currency.selectedIndex].value;
     var secondValue = currency2.options[currency2.selectedIndex].value;
    resutText.innerHTML =  value+" - " + secondValue;
+   if(secondValue == "GBP"){
+    secondImage.src = "https://img2.pngindir.com/20180322/ide/kisspng-england-kingdom-of-great-britain-flag-of-the-unite-cyber-nations-wiki-5ab40918aab721.6894237315217482486993.jpg";
+   }
+   else if(secondValue == "EUR"){
+    secondImage.src =   "https://ulkelerbayraklar.com/wp-content/uploads/2017/12/almanya_bayrag%CC%86%C4%B1-800x445.png";
+    }
+    else if(secondValue == "USD"){
+        secondImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"
+    }
+    else if(secondValue == "TRY"){
+        secondImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png"
+    }
+    else if(secondValue == "JPY"){
+        secondImage.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/200px-Flag_of_Japan.svg.png"
+    }
    returnValue();
+
 }
 
 
@@ -32,7 +65,7 @@ async function returnValue(){
     const responsedata = await response.json();
 
     console.log(Object.values(responsedata)[0]);
-    resultValue.innerHTML = Object.values(responsedata)[0] * amount.value;
+    resultValue.innerHTML = (Object.values(responsedata)[0] * amount.value).toFixed(2);
     
 }
 
